27d14ab36be66662c90a59e6f7c1cab09bb89183
Abhay Bajaj <abhaybajajit@gmail.com>
2021-12-30 12:54:35 +0530

    Backend created using sanity

---
+++ backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default{
+    name:'user',
+    titile:'User',
+    type:'document',
+    fields:[
+        {
+            name:'username',
+            title:'UserName',
+            type:'string'
+        },
+        {
+            name:'image',
+            title:'Image',
+            type:'string'
+        }
+    ]
+}
