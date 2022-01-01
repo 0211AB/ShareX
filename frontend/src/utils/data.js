a368983c106e42d3081b88eaff10da6b6e45d0b0
Abhay Bajaj <abhaybajajit@gmail.com>
2022-01-01 10:06:19 +0530

    User Profile Page created

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -166,3 +166,58 @@ export const feedQuery = `*[_type == "post"] | order(_createdAt desc) {
           },
         },
       } `;
+
+export const userCreatedpostsQuery = (userId) => {
+  const query = `*[ _type == 'post' && userId == '${userId}'] | order(_createdAt desc){
+          image{
+            asset->{
+              url
+            }
+          },
+          _id,
+          destination,
+          postedBy->{
+            _id,
+            userName,
+            image
+          },
+          save[]{
+            postedBy->{
+              _id,
+              userName,
+              image
+            },
+          },
+        }`;
+  return query;
+};
+
+export const userQuery = (userId) => {
+  const query = `*[_type == "user" && _id == '${userId}']`;
+  return query;
+};
+
+export const userSavedpostsQuery = (userId) => {
+  const query = `*[_type == 'post' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+          image{
+            asset->{
+              url
+            }
+          },
+          _id,
+          destination,
+          postedBy->{
+            _id,
+            userName,
+            image
+          },
+          save[]{
+            postedBy->{
+              _id,
+              userName,
+              image
+            },
+          },
+        }`;
+  return query;
+};
