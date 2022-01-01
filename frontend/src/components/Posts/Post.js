d461f454762671ccf072bf2354501f4bbd32f67c
Abhay Bajaj <abhaybajajit@gmail.com>
2022-01-01 08:23:26 +0530

    Single post component created

--- frontend/src/components/Posts/Post.js
+++ frontend/src/components/Posts/Post.js
@@ -1,12 +1,92 @@
-import React from 'react'
-import { urlFor } from '../../client'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import { MdDownloadForOffline } from 'react-icons/md';
+import { AiTwotoneDelete } from 'react-icons/ai';
+import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
+import { urlFor, client } from '../../client'
 
-const Post = ({ post: { postedBy, image, _id, destination } }) => {
+const Post = ({ post }) => {
+
+    const [postHovered, setPostHovered] = useState(false);
+    const [savingPost, setSavingPost] = useState(false);
+
+    const { postedBy, image, _id, destination } = post;
+
+    const user = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+    let alreadySaved = post?.save?.filter((item) => item?.postedBy?._id === user?.googleId);
+    alreadySaved = alreadySaved?.length > 0 ? alreadySaved : [];
+
+    const navigate = useNavigate();
+
+    const savePost = (id) => {
+        if (alreadySaved?.length === 0) {
+            setSavingPost(true);
+
+            client
+                .patch(id)
+                .setIfMissing({ save: [] })
+                .insert('after', 'save[-1]', [{
+                    _key: uuidv4(),
+                    userId: user?.googleId,
+                    postedBy: {
+                        _type: 'postedBy',
+                        _ref: user?.googleId,
+                    },
+                }])
+                .commit()
+                .then(() => {
+                    window.location.reload();
+                    setSavingPost(false);
+                });
+        }
+    };
+
+    const deletePost = (id) => {
+        client
+            .delete(id)
+            .then(() => {
+                window.location.reload();
+            });
+    };
+
     return (
-        <div>
-            <img className='rounded-lg w-full' src={urlFor(image).width(250).url()}></img>
-        </div>
-    )
+        <div className="m-2">
+            <div onMouseEnter={() => setPostHovered(true)}
+                onMouseLeave={() => setPostHovered(false)}
+                onClick={() => navigate(`/post-detail/${_id}`)}
+                className=" relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out">
+                <img className="rounded-lg w-full " src={(urlFor(image).width(250).url())} alt="user-post" />
+                {postHovered && (
+                    <div className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50" style={{ height: '100%' }}>
+                        <div className="flex items-center justify-between">
+                            <div className="flex gap-2">
+                                <a href={`${image?.asset?.url}?dl=`} download onClick={(e) => { e.stopPropagation(); }} className="bg-white w-9 h-9 p-2 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-mdoutline-none"><MdDownloadForOffline /> </a>
+                            </div>
+
+                            {alreadySaved?.length !== 0 ? (<button type="button" className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none">{post?.save?.length}  Saved </button>
+                            ) : (
+                                <button onClick={(e) => { e.stopPropagation(); savePost(_id); }} type="button"
+                                    className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none">
+                                    {post?.save?.length}   {savingPost ? 'Saving' : 'Save'}</button>
+                            )}
+                        </div>
+
+                        {postedBy?._id === user?.googleId && (
+                            <button type="button" onClick={(e) => { e.stopPropagation(); deletePost(_id); }}
+                                className="bg-white p-2 rounded-full w-8 h-8 flex items-center justify-center text-dark opacity-75 hover:opacity-100 outline-none">
+                                <AiTwotoneDelete /></button>
+                        )}
+
+                    </div>
+                )}
+            </div>
+            <Link to={`/user-profile/${postedBy?._id}`} className="flex gap-2 mt-2 items-center">
+                <img className="w-8 h-8 rounded-full object-cover" src={postedBy?.image} alt="user-profile" />
+                <p className="font-semibold capitalize ">{postedBy?.username}</p>
+            </Link>
+        </div>)
 }
 
 export default Post
